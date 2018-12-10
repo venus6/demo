ca7bc40f77d455cbba2f2bb9725092e2922b450c
venus <venuskings@outlook.com>
2018-12-10 21:30:47 +0800

    modify

--- system/core/URI.php
+++ system/core/URI.php
@@ -211,9 +211,9 @@ class CI_URI {
 		{
 			if (strpos($uri, $_SERVER['SCRIPT_NAME']) === 0)
 			{
-				if (substr($_SERVER['SCRIPT_NAME'], -4) != '.php') {
+				/*if (substr($_SERVER['SCRIPT_NAME'], -4) != '.php') {
 					var_dump($uri);var_dump($_SERVER['SCRIPT_NAME']);exit;
-				}
+				}*/
 				$uri = (string) substr($uri, strlen($_SERVER['SCRIPT_NAME']));
 
 			}
