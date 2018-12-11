43dc61dcb2feb3e8e0336ca07e22b9789e652a82
venus <venuskings@outlook.com>
2018-12-11 20:57:33 +0800

    modify

--- system/core/URI.php
+++ system/core/URI.php
@@ -206,7 +206,7 @@ class CI_URI {
 		$uri = parse_url('http://dummy'.$_SERVER['REQUEST_URI']);
 		$query = isset($uri['query']) ? $uri['query'] : '';
 		$uri = isset($uri['path']) ? $uri['path'] : '';
-var_dump($uri);var_dump($_SERVER['SCRIPT_NAME']);exit;
+//var_dump($uri);var_dump($_SERVER['SCRIPT_NAME']);exit;
 		if (isset($_SERVER['SCRIPT_NAME'][0]))
 		{
 			if (strpos($uri, $_SERVER['SCRIPT_NAME']) === 0)
