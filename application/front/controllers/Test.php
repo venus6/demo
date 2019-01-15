78457e8b05d274076d8cc2df678277d6bfe0f8f4
venus <venuskings@outlook.com>
2019-01-15 14:11:57 +0800

    modify

--- application/front/controllers/Test.php
+++ application/front/controllers/Test.php
@@ -59,6 +59,7 @@ class Test extends CI_Controller {
     public function redis() {
         $redis = new Redis();
         $redis->connect('127.0.0.1', 6379);
+        $redis->set('name', 'venus');
         $str = $redis->get('two');
         var_dump($str);
     }
